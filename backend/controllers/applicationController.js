04dabecfbac6d1fb663c5100fa8361ea639b74c5
Akaash Raj M <akaashraj.26it@licet.ac.in>
2025-03-14 09:43:10 +0530

    Update commit

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -1,12 +1,14 @@
 const Application = require("../models/Application");
 
-exports.applyForJob = async (req, res) => {
-   const { candidate, job } = req.body;
-   try {
-       const newApplication = new Application({ candidate, job });
-       await newApplication.save();
-       res.json({ message: "Application submitted successfully" });
-   } catch (error) {
-       res.status(500).json({ error: "Server error" });
-   }
+const getUserApplications = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const applications = await Application.find({ userId });
+    res.json(applications);
+  } catch (error) {
+    res.status(500).json({ error: "Server Error" });
+  }
 };
+
+module.exports = { getUserApplications };
+
